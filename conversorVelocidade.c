ed3ca4bc3b033bb9dccb7d7d19f74d03ca71ddc0
Rian Barroso <rianbogferreira@gmail.com>
2024-12-22 18:12:00 -0300

    Calculo para conversoes

--- conversorVelocidade.c
+++ conversorVelocidade.c
@@ -4,3 +4,8 @@ void converterVelocidade() {
     printf("\n--- Conversor de Velocidade ---\n");
     printf("Digite a velocidade em km/h: ");
     scanf("%lf", &valor);
+
+    kmh = valor;
+    ms = kmh / 3.6;
+    mph = kmh / 1.609;
+    
