d00cfe3dac526660fc5f076583916be2b1d3d3d2
Rian Barroso <rianbogferreira@gmail.com>
2024-12-22 18:10:32 -0300

    Estrutura basica da funçao e solicita o valor que deseja converter.

---
+++ conversorVelocidade.c
@@ -0,0 +1,6 @@
+void converterVelocidade() {
+    double valor, kmh, ms, mph;
+
+    printf("\n--- Conversor de Velocidade ---\n");
+    printf("Digite a velocidade em km/h: ");
+    scanf("%lf", &valor);
