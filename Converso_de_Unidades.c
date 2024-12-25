a2e7141b572575f2e4d889e9ce32cc5db6258415
Cauã Brizolara <cauabrizolara@hotmail.com>
2024-12-25 00:59:47 -0300

    commit-caua ConversorComprimento

--- Converso_de_Unidades.c
+++ Converso_de_Unidades.c
@@ -4,46 +4,7 @@
 
 // Estrutura do MENU do Código
 
-void ConversorComprimento()
-{
-    int opcaocomp;
-    float metro, resultado;
 
-        printf("Conversão de Comprimento:\n");
-        printf("1. Metro para Centímetro\n");
-        printf("2. Metro para Milímetro\n");
-        printf("3. Centimetro para Metro\n");
-        printf("4. Milimetro para Metro\n");
-        scanf("%d", &opcaocomp);
-        printf("Digite o valor que deseja converter: \n");
-        scanf("%f", &metro);
-
-        if (opcaocomp == 1)
-        {
-            resultado = metro * 100; // metro para cm
-            printf("O valor é: %.2f cm \n", resultado);
-        }
-        else if (opcaocomp == 2)
-        {
-            resultado = metro * 1000; // metro para mm
-
-            printf("O valor é: %.2f mm \n", resultado);
-        }
-        else if (opcaocomp == 3)
-        {
-            resultado = metro / 100; // cm para metro
-
-            printf("O valor é: %.2f m \n", resultado);
-        }
-        else if (opcaocomp == 4)
-        {
-            resultado = metro / 1000; // mm para metro
-
-            printf("O valor é: %.2f m \n", resultado);
-        } else {
-            printf("Opção inválida!\n");
-        }
-    }
 
 void ConversorEnergia()
 { // Função para Converte de Watts para Quilowatts
